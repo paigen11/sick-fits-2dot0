8acc48253559b876a1cbad71e0ee293b409d127a
Paige Niedringhaus <paigen11@gmail.com>
2021-04-01 09:07:10 -0400

    updating general app to have loading bar switching between pages

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,13 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+// how to hook into router and listen for events in next.js
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
