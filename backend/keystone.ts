deb47f34d98feba5dca1c5d7e6381be4d73d0ec1
Paige Niedringhaus <paigen11@gmail.com>
2021-05-03 17:35:07 -0400

    hooking up seed data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -39,7 +40,11 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseUrl,
-      // todo add data seeding here
+      async onConnect(keystone) {
+        console.log('connected to the db')
+        if(process.argv.includes('--seed-data'))
+        await insertSeedData(keystone)
+      }
     },
     lists: createSchema({
       // schema items go here
